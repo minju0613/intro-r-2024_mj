4cd772d47c32c642e15091687d7f546422d61061
minju0613 <kminju@pdx.edu>
2024-09-10 16:21:59 -0700

    intro to data frames

--- R/r_basics.R
+++ R/r_basics.R
@@ -100,10 +100,16 @@ all(a < 3) # tests whether all comparisons TRUE
 
 # data frames - the key structure for data science, multi-dimensional
 #   collections of vectors
+df <- data.frame(a = c(1, 2, 3), 
+                 b = c("minju", "eugene", "woojung")) # collection of vectors
+df
 
+df$a  # references single column
+df$b  
+
+df$mode <- c("bike", "car", "bus") # adding a column
+summary(df) # summarizes by column
 
 # Special type: factors, and putting it all together ----
 # factors are categorical variables with a fixed set of
 #   potential values
-
-
